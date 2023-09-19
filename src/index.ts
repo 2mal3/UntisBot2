8c7ae052448f23a99b4a3bae038f73b6f87406fa
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-19 21:54:07 +0200

    chore: just shoot me

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,65 @@
-import { CronJob } from 'cron'
-import { Client, GatewayIntentBits, Partials, TextChannel } from "discord.js";
+import { Logger, ILogObj } from "tslog";
+import { CronJob } from "cron";
+import {
+  Client,
+  REST,
+  GatewayIntentBits,
+  Partials,
+  TextChannel,
+  Routes,
+} from "discord.js";
+
+const log: Logger<ILogObj> = new Logger({
+  prettyLogTemplate: "{{yyyy}}.{{mm}}.{{dd}} {{hh}}:{{MM}}:{{ss}}:{{ms}}\t{{logLevelName}}\t",
+});
 
 const bot = new Client({
   intents: [GatewayIntentBits.Guilds],
   partials: [Partials.Channel],
 });
 
-function main() {
-  console.log("hi");
+async function register_commands() {
+  const commands = [
+    {
+      name: "ping",
+      description: "Replies with Pong!",
+    },
+  ];
+
+  const rest = new REST({ version: "10" }).setToken(
+    Bun.env.DISCORD_TOKEN ?? ""
+  );
+
+  try {
+    log.debug("Started refreshing application (/) commands");
+
+    await rest.put(
+      Routes.applicationCommands(Bun.env.DISCORD_APPLICATION_ID ?? ""),
+      { body: commands }
+    );
+
+    log.debug("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    log.error(error);
+  }
 }
 
-// bot.login(process.env.DISCORD_TOKEN);
-let job = new CronJob('* * * * *', main, null, true, process.env.TZ)
+bot.on("ready", async () => {
+  log.info("Bot connected!");
+});
+
+bot.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName == "ping") {
+    await interaction.reply("Pong!");
+  }
+});
+
+function main() {
+  log.info("Hi");
+}
+
+await register_commands();
+bot.login(process.env.DISCORD_TOKEN);
+const job = new CronJob("* * * * *", main, null, true, Bun.env.TZ);
