02f17fb6c6a2de495ec1ab12d1a6835caf9ca7b7
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-09-20 19:17:30 +0200

    chore: auto get actual school name + server

--- src/logic.ts
+++ src/logic.ts
@@ -1,3 +1,4 @@
+import puppeteer from "puppeteer";
 import { WebAPITimetable, WebUntis } from "webuntis";
 import { log } from "logging";
 
@@ -9,5 +10,26 @@ export async function register_user(
   log.debug(username, password, school_name);
 }
 
-async function get_school_from_name(name: string) {
+async function get_school_from_name(
+  name: string
+): Promise<{ school_name: string; untis_server: string }> {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+
+  await page.goto("https://webuntis.com/");
+
+  const search_input_selector = ".Select-placeholder";
+  await page.waitForSelector(search_input_selector);
+  await page.type(search_input_selector, name);
+
+  const search_option_selector = ".search-option";
+  await page.waitForSelector(search_option_selector);
+  await page.click(search_option_selector);
+
+  const untis_sever = page.url().split("/")[2];
+  const school_name = page.url().split("/")[4].split("=")[1].replace("+", " ");
+
+  await browser.close();
+
+  return { school_name: school_name, untis_server: untis_sever };
 }
