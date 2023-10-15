b4e9cad92148764f6c11309f70b8d53ae1f183ae
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-10-15 11:22:09 +0200

    ref: move untis logic to separate file

--- src/logic.ts
+++ src/logic.ts
@@ -1,7 +1,8 @@
 import { Database } from "bun:sqlite";
-import { WebAPITimetable, WebUntis } from "webuntis";
+import { WebUntis } from "webuntis";
 import { log } from "logging";
 import { Lesson, User } from "types";
+import { get_school_from_name, get_timetable } from "untis"
 import { v4 as uuid4 } from "uuid";
 
 export async function user_login(
@@ -58,95 +59,6 @@ export async function user_login(
   return { success: true, message: "" };
 }
 
-// Get the untis internal school name and the server url
-// from the school search on the webuntis website
-export async function get_school_from_name(
-  name: string
-): Promise<{ school_name: string; untis_server: string }> {
-  const response = await fetch("https://mobile.webuntis.com/ms/schoolquery2", {
-    method: "POST",
-    body: JSON.stringify({
-      id: "",
-      method: "searchSchool",
-      params: [
-        {
-          search: name,
-        },
-      ],
-      jsonrpc: "2.0",
-    }),
-  });
-  const data = await response.json();
-
-  if (data.result.schools.length === 0) {
-    throw new Error("No schools found");
-  }
-
-  const school_name = data.result.schools[0].loginName;
-  const untis_sever = data.result.schools[0].server;
-
-  return { school_name: school_name, untis_server: untis_sever };
-}
-
-function create_date_from_untis_date(
-  untis_date: number,
-  untis_time: number
-): Date {
-  const untis_date_string = untis_date.toString();
-
-  const year = untis_date_string.slice(0, 4);
-  const month = untis_date_string.slice(4, 6);
-  const day = untis_date_string.slice(6, 8);
-  const hour = Math.floor(untis_time / 100)
-    .toString()
-    .padStart(2, "0");
-  const minute = (untis_time % 100).toString().padStart(2, "0");
-
-  const date_string = `${year}-${month}-${day}T${hour}:${minute}:00.000Z`;
-  const date = new Date(date_string);
-
-  return date;
-}
-
-async function get_timetable(user: User): Promise<WebAPITimetable[]> {
-  const untis = new WebUntis(
-    user.untis_school_name,
-    user.untis_username,
-    user.untis_password,
-    user.untis_server
-  );
-
-  await untis.login();
-  const timetable = await untis.getOwnTimetableForWeek(new Date());
-  await untis.logout();
-
-  return timetable;
-}
-
-function format_timetable(timetable: WebAPITimetable[]): Lesson[] {
-  let nice_timetable: Lesson[] = [];
-
-  for (const lesson of timetable) {
-    let nice_lesson: Lesson = {
-      id: lesson.id,
-      name: lesson.subjects[0].element.longName ?? "missingno",
-      date: create_date_from_untis_date(lesson.date, lesson.startTime),
-      cancelled: false,
-    };
-    // Ignore this error, it's a bug in the typings
-    if (
-      (lesson.is.cancelled ?? false) ||
-      lesson.teachers[0].element.name === "---"
-    ) {
-      nice_lesson.cancelled = true;
-    }
-
-    nice_timetable.push(nice_lesson);
-  }
-
-  return nice_timetable;
-}
-
 // Loops thought the timetable by index and prints out every lesson where the cancelled status has changed
 export function filter_cancelled_lessons(
   timetable: Lesson[],
@@ -164,7 +76,7 @@ export function filter_cancelled_lessons(
 }
 
 export async function get_cancelled_lessons(db: Database, user: User): Promise<Lesson[]> {
-  const nice_new_timetable = format_timetable(await get_timetable(user));
+  const nice_new_timetable = await get_timetable(user);
 
   // Create timetable if it doesn't exist
   const user_timetable = JSON.parse(user.timetable) as Lesson[];
