6eb62dd43cee33d560bb0bd5bb88b05c6a775730
2mal3 <56305732+2mal3@users.noreply.github.com>
2023-10-15 19:06:58 +0200

    test: update with last refactor

--- tests/logic.test.ts
+++ tests/logic.test.ts
@@ -1,19 +1,6 @@
 import { expect, test } from "bun:test";
-import { get_school_from_name, filter_cancelled_lessons } from "logic";
+import { filter_cancelled_lessons } from "logic";
 import { Lesson } from "types";
-
-test("get_existing_school_from_name", async () => {
-  expect(await get_school_from_name("abc")).toEqual({
-    school_name: "Mysen skole",
-    untis_server: "korfu.webuntis.com",
-  });
-});
-
-test("get_nonexisting_school_from_name", async () => {
-  expect(async () => {
-    await get_school_from_name("qqqqqqqqqqq");
-  }).toThrow("No schools found");
-});
 
 test("filter_canceled_lessons", () => {
   const date_string = "2023-10-12T10:10:00.000Z";
---
+++ tests/untis.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "bun:test";
+import { get_school_from_name } from "untis";
+
+test("get_existing_school_from_name", async () => {
+  expect(await get_school_from_name("abc")).toEqual({
+    school_name: "Mysen skole",
+    untis_server: "korfu.webuntis.com",
+  });
+});
+
+test("get_nonexisting_school_from_name", async () => {
+  expect(async () => {
+    await get_school_from_name("qqqqqqqqqqq");
+  }).toThrow("No schools found");
+});
